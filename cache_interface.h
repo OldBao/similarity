3ec4bea9b556ca1cd076a194d768993e10f2c912
gethinzhang <zgxcassar@gmail.com>
2015-01-19 23:52:02 +0800

    add cache interface

--- cache_interface.h
+++ cache_interface.h
@@ -1,1 +1,288 @@
-/home/users/zhangguanxing01/scmpf/app/mobile/flyflow/server/cbs/lib.bak/output/cbsinclude/cache_interface.h
+/*
+ * cache_namespace.h
+ *
+ *  Created on: 2012-8-13
+ *      Author: jx.peng
+ */
+
+#ifndef CACHE_INTERFACE_H_
+#define CACHE_INTERFACE_H_
+#include <stdint.h>
+#include <nshead.h>
+
+/*
+ * declare the cache operation type for dispatch different kinds of
+ * cache items.
+ */
+typedef enum cache_type
+{
+    basicItem   = 0x0,
+    finalItem   = 0x10,
+    sFinalItem  = 0x11,
+    clipFinal   = 0x12,
+    sClipFinal  = 0x13,
+    incrData    = 0x14,
+    sIncrData   = 0x15,
+
+    finalList   = 0x20,
+    sFinalList  = 0x21,
+    cmtFinalList = 0x22,
+
+    mfFinal     = 0x30,
+    sMfFinal    = 0x31,
+    mediItem    = 0x32,
+    sMediItem   = 0x33,
+
+    finalXml    = 0x40,
+    sFinalXml   = 0x41,
+
+	finalIndividual = 0x50,
+	finalSIndividual = 0x51
+} cache_t;
+
+typedef enum CACHE_ITEM_TYPE{
+    FINAL_HTML = 0x1,
+    FINAL_MAIN_HTML,
+    FINAL_COOKIE,
+    FINAL_LIST_PURE,
+    FINAL_LIST_ALL
+}cache_item_t;
+
+/* operation id for basic cache operation*/
+enum CacheCommand{
+    CMD_CACHE_SEEK = 0,
+    CMD_CACHE_ADD = 1,
+    CMD_CACHE_DEL = 2,
+    CMD_CACHE_RANGE_REMOVE_NAMESPACE = 3,
+    CMD_CACHE_UPDATE_WITH_PREV_EXPIRE = 4,
+    CMD_CACHE_APPEND = 5,                   //cache will handle it with lock
+    CMD_CACHE_SEEK_LOCK = 6,                //cache will handle it with lock
+    CMD_CACHE_ADD_LOCK = 7,                 //cache will handle it with lock
+    CMD_CACHE_DEL_LOCK = 8,                 //cache will handle it with lock
+    CMD_CACHE_INCREASE = 9,                 //regard it as integer(int64), cache will handle it with lock
+    CMD_CACHE_TABLE_OP = 10,
+    CMD_CACHE_OP = 11,                      //duplicate
+    CMD_CACHE_MAX
+};
+
+enum CacheAppendCommand{
+    CacheListAppend,
+    CacheListAdd,
+    CacheListDel,
+    CacheListMix
+};
+
+enum CacheListMixType{
+    listMixAdd,
+    listMixDel
+};
+
+#define CMD_CACHE_LIST_APPEND ((CacheListAppend << 4) | CMD_CACHE_APPEND)
+#define CMD_CACHE_LIST_ADD ((CacheListAdd << 4) | CMD_CACHE_APPEND)
+#define CMD_CACHE_LIST_DEL ((CacheListDel << 4) | CMD_CACHE_APPEND)
+#define CMD_CACHE_LIST_MIX ((CacheListMix << 4) | CMD_CACHE_APPEND)
+
+enum CacheOpCommand{
+    CacheStartDup,
+    CacheStartTransfer,
+    CacheDup,
+    CacheTransfer
+};
+
+#define CMD_CACHE_OP_START_DUP ((CacheStartDup << 4) | CMD_CACHE_OP)
+#define CMD_CACHE_OP_START_TRANSFER ((CacheStartTransfer << 4) | CMD_CACHE_OP)
+#define CMD_CACHE_OP_DUP ((CacheDup << 4) | CMD_CACHE_OP)
+#define CMD_CACHE_OP_TRANSFER ((CacheTransfer << 4) | CMD_CACHE_OP)
+
+
+#define CMD_CACHE_MODIFY_BIT        1
+
+/* cache operation id for full browsing */
+#define CMD_CACHE_MODIFY_ADD            ((CMD_CACHE_MODIFY_BIT << 4) | CMD_CACHE_ADD)
+
+#define CMD_CACHE_SEEK_FINAL            ((finalItem << 4) | CMD_CACHE_SEEK) /*0x101*/
+#define CMD_CACHE_ADD_FINAL_ITEM	    ((finalItem << 4) | CMD_CACHE_ADD)  /*0102*/
+
+#define CMD_CACHE_ADD_FINAL_LIST	    ((finalList << 4) | CMD_CACHE_ADD) /*0x202 */
+
+#define CMD_CACHE_ADD_MF_FINAL          ((mfFinal << 4) | CMD_CACHE_ADD) /*0x302 */
+/* for rewrite the intermediate results */
+#define CMD_CACHE_ADD_MEDI              ((mediItem << 4) | CMD_CACHE_ADD) /*0x322*/
+#define CMD_CACHE_SEEK_MF_FINAL         ((mfFinal << 4) | CMD_CACHE_SEEK) /*0x301 */
+
+#define CMD_CACHE_SEEK_XML_FINAL        ((finalXml << 4) | CMD_CACHE_SEEK) /*0x401 */
+#define CMD_CACHE_ADD_XML_FINAL         ((finalXml << 4) | CMD_CACHE_ADD) /*0x402 */
+/* clip list */
+#define CMD_CACHE_SEEK_CLIPS_FINAL      ((clipFinal << 4) | CMD_CACHE_SEEK) /*0x121*/
+
+/* increase data */
+#define CMD_CACHE_SEEK_INCR_DATA 		((incrData << 4) | CMD_CACHE_SEEK) // 0x141
+#define CMD_CACHE_ADD_INCR_DATA	        ((incrData << 4) | CMD_CACHE_ADD)  /*0x142*/
+
+/* final individuals */
+#define CMD_CACHE_SEEK_FINAL_INDIVIDUAL ((finalIndividual << 4) | CMD_CACHE_SEEK) /* 0x501 */
+#define CMD_CACHE_SEEK_FINAL_SINDIVIDUAL ((finalSIndividual << 4) | CMD_CACHE_SEEK) /* 0X511 */
+
+
+
+/* cache operation id for simple browsing */
+#define CMD_CACHE_SEEK_SFINAL		    ((sFinalItem << 4) | CMD_CACHE_SEEK) /*0x111  */
+#define CMD_CACHE_ADD_FINAL_SITEM	    ((sFinalItem << 4) | CMD_CACHE_ADD) /*0x112 */
+
+#define CMD_CACHE_ADD_FINAL_SLIST	    ((sFinalList << 4) | CMD_CACHE_ADD) /*0x212  */
+
+#define CMD_CACHE_ADD_SMF_FINAL         ((sMfFinal << 4) | CMD_CACHE_ADD)  /*0x312  */
+/* for rewrite the intermediate results for simple browsing */
+#define CMD_CACHE_ADD_SMEDI             ((sMediItem << 4) | CMD_CACHE_ADD) /*0x332 */
+#define CMD_CACHE_SEEK_SMF_FINAL        ((sMfFinal << 4) | CMD_CACHE_SEEK) /*0x311 */
+
+#define CMD_CACHE_SEEK_SXML_FINAL       ((sFinalXml << 4) | CMD_CACHE_SEEK) /*0x411  */
+#define CMD_CACHE_ADD_SXML_FINAL        ((sFinalXml << 4) | CMD_CACHE_ADD) /*0x412  */
+/* clip list */
+#define CMD_CACHE_SEEK_CLIPS_SFINAL     ((sClipFinal << 4) | CMD_CACHE_SEEK) //0x131
+
+/* increase data */
+#define CMD_CACHE_SEEK_SINCR_DATA 		((sIncrData << 4) | CMD_CACHE_SEEK) //0x151
+#define CMD_CACHE_ADD_SINCR_DATA	    ((sIncrData << 4) | CMD_CACHE_ADD)  //0x152
+
+/* commit list */
+#define CMD_CACHE_SEEK_CMT_FINAL_LIST	((cmtFinalList << 4) | CMD_CACHE_SEEK)  //0x221
+
+
+#define CMD_CACHE_END				0x900 /* end of the cmd */
+
+
+const static char LIST_SPLITER[] = "\r\n";
+const static uint32_t LIST_SPLITER_SIZE = sizeof(LIST_SPLITER) - 1;
+
+const static char TABLE_ADD_KEY[] = "tableadd";
+const static char TABLE_MAX_KEY[] = "tablemax";
+const static char TABLE_KEYWORDS_KEY[] = "keyword";
+const static char TABLE_TICK_KEY[] = "tick";
+const static char TABLE_USER_SUB_KEY[] = "user_sub";
+const static uint32_t TABLE_ADD_KEY_SIZE = sizeof(TABLE_ADD_KEY) - 1;
+const static char TABLE_DEL_KEY[] = "tabledel";
+const static uint32_t TABLE_DEL_KEY_SIZE = sizeof(TABLE_DEL_KEY) - 1;
+
+enum CacheRetCode{
+    CacheTableInputError = -7,
+    CacheListResultEmtpy = -6, //in fact, it represent success
+    CacheListMixParaError = -5,
+    CacheListRmEmpty = -4,
+    CacheAppendOverFlow = -3,
+    CacheSizeError = -2,
+    CacheOperationError = -1,
+    CacheOperationSuccess = 0,//for compatible, must equal to CACHE_RET_HIT
+    CacheSeekNoHit = 1 //for compatible, must equal to CACHE_RET_NOTHIT
+};
+
+/*
+ * define the url tag for full and simple browsing
+ */
+/* full browsing */
+#define cache_url_final_tag                         "{f"
+#define cache_url_final_list_tag                    "{"
+#define cache_url_mf_tag                            "{m"
+#define cache_url_xml_final_tag                     "("
+
+#define cache_url_final_list_char_tag               '{'
+#define cache_url_xml_final_char_tag                '('
+
+/* simple browsing */
+#define cache_url_sfinal_tag                        "}f"
+#define cache_url_final_slist_tag                   "}"
+#define cache_url_smf_tag                           "}m"
+#define cache_url_sxml_final_tag                    ")"
+
+#define cache_url_final_slist_char_tag              '}'
+#define cache_url_sxml_final_char_tag               ')'
+
+/* common separator */
+#define cache_url_sep                               "\n"
+
+/** cookie url tag for cloud cookie storage */
+#define cache_cookie_url_tag ".cookie_dxpack"
+
+enum CacheNamespace{
+	PlainNamespace = 1,
+	RawImageNamespace = 2,
+	ProcessedImageNamespace = 3,
+	TopicNamespace = 4,
+	SubscribeListNamespace = 5,
+	WordFrequencyNamespace = 6,
+	CrawlerNamespace = 7,
+	SubscribeKwListNamespace = 8,
+    FrameNameSpace = 9,//for frame
+    FrameRangeNameSapce = 10,//for frame
+    AssembleMetaNameSpace = 11,//for gate
+    ReadingUpdaterNameSpace = 12,//for home user updater
+    ReadingFinalRangeNameSpace = 13,//user will see it
+    ReadingFinalBlockNameSpace = 14,//user will see it
+    ReadingFinalGroupNameSpace = 15,//user will see it
+    ReadingSecondRangeNameSpace = 16,
+    ReadingSecondNameSpace = 17,
+    ReadingNormalNameSpace = 18,
+    ReadingNormalRangeNameSpace = 19,
+    UserTableNameSpace = 25,
+    KeyWordTableNameSpace = 26,
+    SidTableNameSpace = 27,
+    CacheNameSpaceMax = 40
+};
+
+#define _BDTC_MAX_URL_LEN 1024
+
+struct cache_req_t{
+    cache_req_t() : name_space(PlainNamespace){}
+    //cache timeout, use default time if set to 0, otherwise ms
+    uint32_t age;
+    //for future, now not in use
+    uint32_t bucket;
+    //high 8bit treats as type:
+    //1 : final result
+    uint16_t cmd_id;
+    uint16_t name_space;
+    char key[_BDTC_MAX_URL_LEN];//url key
+    char buf;//extra data if cmd is store
+};
+
+struct cache_res_t{
+    short ret_no; //0 = hit, 1= no hit, 2=download <0 error
+    bool b_body;//whether has extra data, nshead.len-sizeof(cache_res_t)
+};
+
+const static uint32_t CACHE_REQ_HEADER_SIZE = sizeof(cache_req_t);
+const static uint32_t CACHE_RES_HEADER_SIZE = sizeof(cache_res_t);
+
+const static uint32_t CACHE_REQ_CONTENT_MAX_SIZE = (8u<<20) - sizeof(cache_req_t) - sizeof(nshead_t);
+const static uint32_t CACHE_RES_CONTENT_MAX_SIZE = (8u<<20) - sizeof(cache_res_t) - sizeof(nshead_t);
+
+const static uint32_t NO_EXPIRE_TIME = 0xffffffff;
+
+inline bool isCacheNamespaceLegal(int name_space){
+    return name_space >= PlainNamespace && name_space < CacheNameSpaceMax;
+}
+
+inline bool isCacheCmdLegal(int cmd){
+    return cmd >= CMD_CACHE_SEEK && cmd < CMD_CACHE_MAX;
+}
+
+inline bool canIgnoreKey(int cmd){
+    cmd = cmd & 0x0F;
+    if(cmd == CMD_CACHE_RANGE_REMOVE_NAMESPACE || cmd == CMD_CACHE_OP)
+        return true;
+
+    return false;
+}
+
+//below for compatible
+#define CACHE_RET_HIT           0
+#define CACHE_RET_NOTHIT        1
+#define CACHE_RET_DOWNLOAD      2
+#define CACHE_TIMEOUT           3
+#define CACHE_RET_FINAL_COOKIE  4
+
+
+#define CACHE_RETERR_INITIAL   -1
+#define CACHE_RETERR_UNKNOW    -2
+
+#endif /* CACHE_INTERFACE_H_ */
--- interface.h
+++ interface.h
@@ -1,1 +1,199 @@
-/home/users/zhangguanxing01/scmpf/app/mobile/flyflow/server/cbs/lib.bak/output/cbsinclude/interface.h
+#ifndef __INTERFACE_H__
+#define __INTERFACE_H__
+
+#include <sys/types.h>
+#include <stdint.h>
+#include "cache_interface.h"
+
+
+#define _CLIENT_UPLOAD_DATA_TRUNK_URL						1	//客户端上传数据--访问URL
+#define _CLIENT_UPLOAD_DATA_TRUNK_HTTP_HEAD				2	//客户端上传数据--HTTP消息头
+#define _CLIENT_UPLOAD_DATA_TRUNK_JS_FUNCTION				3	//客户端上传数据--JavaScript函数调用
+#define _CLIENT_UPLOAD_DATA_TRUNK_FORM_HEAD				4	//客户端上传数据--HTTP消息FORM表单头
+#define _CLIENT_UPLOAD_DATA_TRUNK_GET_BIG_IMG				5	//客户端上传数据--获取大图
+#define _CLIENT_UPLOAD_DATA_TRUNK_GET_SECCODE_IMG			6	//客户端上传数据--获取验证码
+#define _CLIENT_UPLOAD_DATA_TRUNK_INTERACTIVE				7	//客户端上传数据--交互请求
+#define _CLIENT_UPLOAD_DATA_TRUNK_FORM_BASIC_SE			20	//客户端上传数据--Form简单字段
+#define _CLIENT_UPLOAD_DATA_TRUNK_FORM_EDIT_SE_MULTI		21	//客户端上传数据--Form多行编辑字段
+#define _CLIENT_UPLOAD_DATA_TRUNK_FORM_EDIT_SE_CONT		22	//客户端上传数据--Form编辑字段内容
+#define _CLIENT_UPLOAD_DATA_TRUNK_FORM_UPLOAD_FILE_PROP	23	//客户端上传数据--Form文件上传属性
+#define _CLIENT_UPLOAD_DATA_TRUNK_FORM_UPLOAD_DATA			24	//客户端上传数据--Form文件上传数据
+#define _CLIENT_UPLOAD_DATA_TRUNK_CLIENT_DATA				25	//客户端上传数据--客户端数据（主要交互用)
+#define _CLIENT_UPLOAD_DATA_TRUNK_INER_PARAM				26	//客户端上传数据--与服务器内部交互用的参数
+#define _CLIENT_UPLOAD_DATA_TRUNK_IA_FILE_DATA				27	//客户端上传数据--文件（主要交互用)
+#define _CLIENT_UPLOAD_DATA_TRUNK_CLIENT_DATA_ENCRYPTED	28	//客户端上传数据--客户端加密数据（主要交互用)
+#define _CLIENT_UPLOAD_DATA_TRUNK_FORM_BASIC_SE2			30	//客户端上传数据--Form简单字段，代替20
+#define _CLIENT_UPLOAD_DATA_TRUNK_USER_AGENT				31	//客户端上传的User-Agent信息
+#define _CLIENT_UPLOAD_DATA_TRUNK_CLINT_FONT_INFO			32	//客户端上传的字体信息
+#define _CLIENT_UPLOAD_DATA_TRUNK_HTTP_COOKIE				33  //客户端上传的Cookie信息
+#define _CLIENT_UPLOAD_DATA_TRUNK_CLINT_SETTING   			34  //客户端上传的设置信息
+#define _CLIENT_UPLOAD_DATA_TRUNK_PIC_BUNDLE   			35  //客户端批量图片请求信息
+#define _CLIENT_UPLOAD_DATA_TRUNK_MSG_REPLY   				36  //客户回复弹出窗口回复结果信息
+#define _CLIENT_UPLOAD_DATA_TRUNK_CLIENT_IP    			37  //客户端real ip信息
+#define _CLIENT_UPLOAD_DATA_TRUNK_CLIENT_INFO    			38  //客户端宽度高度IMEI
+#define _CLIENT_UPLOAD_DATA_TRUNK_SUBFRAME_INFO  			39  //ONLINE模式下，SUBFrameInfo
+#define _CLIENT_UPLOAD_DATA_TRUNK_SUBSCRIBE_INFO  			40  //ONLINE模式下，SubScribeInfo
+
+#define _CLIENT_UPLOAD_DATA_TRUNK_CHILD_FRAME_BEGIN		10025	//子frame开始标签
+#define _CLIENT_UPLOAD_DATA_TRUNK_CHILD_FRAME_END			10026	//子frame结束标签
+
+
+enum REQ_TYPE
+{
+    CUTE_LAYOUT,
+    FULL_TXT_LAYOUT,
+    FULL_PIC_LYOUT,
+    MAINTOPIC_LAYOUT,
+    INTEREST_SUBSCRIBE
+};
+
+#define CUTE_W_H_F_C_POSTFIX_FORMAT "%d^%d^%d^%d" //width height font collapse
+#define CUTE_SPLIING_MARK "^"
+#define CUTE_PLATFORM_W_H_F_C_POSTFIX_FORMAT "'%s^%d^%d^%d^%d^%d" //platform, width height font collapse online
+#define CUTE_PAGE_FORMAT "|pageidx=%d^pagesize=%d"
+#define CUTE_PAGE_FORMAT_NONIA "|pagesize=%d"
+#define CUTE_REQ_PAGING_FLAG "|pageidx"
+#define CBS_ONLINE_SUB_FRAME_FORMAT "/cbsonline=%d/fw=%d/fh=%d/originw=%d/originh=%d/rate=%.4f/atomic=%d/src=%s" // frame url, final w, h, fit font, collapse.............only for online mode
+#define CBS_ONLINE_UUID_FORMAT "/uuid=%s"
+#define CBS_ONLINE_TYPE "/cbsonline="
+#define CBS_ONLINE_TAG "bd_cbs_online"
+#define CBS_SUBSCRIBE_TAG "bd_cbs_subscribe"
+#define CBS_ONLINE_BASE "%s/bd_cbs_online/ua=%s/font=%d"
+#define CBS_UA_FORMAT "bd_%d_%d_%s_%s_%s"  //bd_320_240_pc78_1-0-12-4_s1
+#define CUTE_PAGE_INDEX "pageidx"
+
+#define CUTE_FIRST_PAGE_INDEX "1"
+#define CUTE_FIRST_PAGE_INDEX_NUM 1
+
+#define CUTE_INCREMENT_ADD_PATH "/div[1]"
+#define CUTE_INCREMENT_RM_PATH "/div[1]"
+
+#define CUTE_TURN_PAGE_FORM_IDX 50002
+#define CUTE_TURN_PAGE_SELECT_IDX 50001
+#define CUTE_TURN_PAGE_SUBMIT_IDX 50000
+#define CUTE_TURN_PAGE_UUID_IDX 49999
+//#define CUTE_TURN_PAGE_CURPAGE_IDX 49998
+#define CUTE_TURN_PAGE_IDX_STEP 100
+
+#define CUTE_DEFAULT_SIZE 12;
+
+#define MAX_DNS_RES_NUM 8
+
+#define CMD_TCCORE_PROCESS 100
+
+
+#define _BDTC_IA_EVENT_NONE                 (0x0)
+#define _BDTC_IA_EVENT_CLICK                (0x00000001)
+#define _BDTC_IA_EVENT_SUBMIT               (0x00000002)
+#define _BDTC_IA_EVENT_CHANGE               (0x00000004)
+#define _BDTC_IA_EVENT_MOUSEOVER            (0x00000008)
+#define _BDTC_IA_EVENT_MOUSEOUT             (0x00000010)
+#define _BDTC_IA_EVENT_MOUSEMOVE            (0x00000020)
+#define _BDTC_IA_EVENT_MOUSEDOWN            (0x00000040)
+#define _BDTC_IA_EVENT_MOUSEUP              (0x00000080)
+#define _BDTC_IA_EVENT_FOCUS                (0x00000100)
+#define _BDTC_IA_EVENT_BLUR                 (0x00000200)
+
+#define _BDTC_TC_RECT "tc_rect"
+#define _BDTC_TC_RECT_FORMAT "%d,%d,%d,%d"
+#define _BDTC_TC_ORIGIN_RECT "tc_originrect"
+#define _BDTC_TC_ORIGIN_RECT_FORMAT "%d,%d"
+#define _BDTC_TC_XPADDING_RECT "tc_xpadding"
+#define _BDTC_TC_YPADDING_RECT "tc_ypadding"
+#define _BDTC_TC_PADDING_RECT_FORMAT "%d,%d"
+#define _BDTC_TC_RATE "tc_rate"
+#define _BDTC_CUTE_BGIMG_URL_FORMAT "%s?tc-res=8&tc_clip=%s&url=%s"
+#define _BDTC_CUTE_BGIMG_URL_FORMAT_STRETCH "%s?tc-res=8&tc_clip=%s&tc_rect=%d_%d&url=%s"
+#define _BDTC_CUTE_BGIMG_URL_FORMAT_WITH_RATE "%s?tc-res=8&tc_clip=%s&tc_rate=%.4f&url=%s"
+#define _BDTC_CUTE_BGIMG_URL_FORMAT_STRETCH_WITH_RATE "%s?tc-res=8&tc_clip=%s&tc_rect=%d_%d&tc_rate=%.4f&url=%s"
+#define _BDTC_CUTE_IMG_URL_FORMAT "%s?tc-res=8&url=%s"
+#define _BDTC_CUTE_IMG_URL_FORMAT_WITH_RATE "%s?tc-res=8&tc_rate=%.4f&url=%s"
+#define _BDTC_CUTE_STRETCH_STR "tc_stretch"
+#define _BDTC_CUTE_STRETCH_NORMAL 0
+#define _BDTC_CUTE_STRETCH_NINE 1
+#define _BDTC_TC_ONLINE_STYLE "display:block;position:absolute;left:%dpx;top:%dpx;width:%dpx;height:%dpx;"
+
+#define CBS_HTTP_HEADER_TIME_FORMAT "%s, %02d %s %d %02d:%02d:%02d GMT"  //Mon, 04 Jun 2012 09:13:16 GMT
+
+#define CBS_HTML_CACHE_IDENTIFY 4785
+
+enum BdFontLevel{
+	BdFontSmall = 1,
+	BdFontMid,
+	BdFontBig
+};
+
+#define CRAWLER_CACHE_CONTROL "Crawler-cache-control"
+enum CRAWLER_CACHE_CONTROL_TYPE{
+	CRAWLER_CACHE_DEFAULT = 0,
+	CRAWLER_CACHE_ONLY_SEEK,
+	CRAWLER_CACHE_ONLY_ADD,
+	CRAWLER_CACHE_NONE
+};
+
+struct crawler_req_t{
+	short cmd_id; //1 = crawler main page.
+	char req[_BDTC_MAX_URL_LEN];// request header
+	char buf;// body buffer if exists
+};
+
+struct crawler_res_t{
+	short ret_no; //0 = success
+	int  size;//return buffer
+};
+
+struct tc_core_req_t{
+#ifdef __TC_CORE_REQ_EXT__
+	bool b_add_cache;//whether to cache, default true
+	bool b_from_cache;//whether the data from cache, default true
+	bool b_notifycache;//whether html page should be return, default false
+	char	 cacheIP[3][32];//CacheIP list, no more than 3
+	u_int32_t cachePort;//cache port
+#endif
+    char req_id;
+	char cache_key[512];//cache key
+	u_int32_t buf_len;// length of URL,key->value in datapack
+#ifdef __TC_CORE_REQ_EXT__
+	char buf;//datapack buffer
+#endif
+};
+
+struct core_res_t{
+	u_int32_t ret_no;//0=succ, otherwise fail
+	u_int32_t age;//cache time, 0 for not to cache, otherwise cache time(ms)
+	char content_type[64];//content-Type value
+	char http_header_ext[512];//extra data in http response header
+};
+
+struct dns_res_t{
+	unsigned int sign[2];
+	int     h_addrtype;     /* host address type */
+	int     h_length;       /* length of address */
+	int 	res_num;//num of h_addr_list
+	char    h_addr_list[MAX_DNS_RES_NUM][32];//use to store result, no more then 32
+};
+
+
+enum CBSOnlineType{
+	CBSOnlineCuteNormal = 1,
+	CBSOnlineCuteSubFrame = 2,
+	CBSOnlineCuteInteractive = 3,
+	CBSOnlineFullNormal = 4,
+	CBSOnlineFullSubFrame = 5,
+	CBSOnlineFullInteractive = 6,
+	CBSOnlineMainTopic = 7,
+	CBSOnlineSubscribe = 8,
+	CBSOnlineTypeNum
+};
+
+enum CoreReqType
+{
+	CoreReqErrorType = 0,
+	CoreReqNormal = 1,
+	CoreReqSubScribe = 2
+};
+
+#endif
+
+
+
